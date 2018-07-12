42f7d06b0548a0e4089e4e14a8b7fbedb7f4e4be
oliviadugger <oliviacdugger@gmail.com>
2018-07-12 14:13:23 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,34 @@
 #write out your code here
 
 def least_coins(cents)
+  
+least_coins_hash = {:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
 
-#Code your answer here!
-
+  while cents >= 25
+  least_coins_hash[:quarters] += 1
+  cents = cents - 25
+  end
+  
+  while cents >= 10
+  least_coins_hash[:dimes] += 1
+  cents = cents - 10
+  end
+  
+  while cents >= 5
+  least_coins_hash[:nickel] += 1
+  cents = cents - 5
+  end
+  
+  while cents >= 1
+  least_coins_hash[:pennies] += 1
+  cents = cents - 1
+  end
+return least_coins_hash
 end
+
+puts least_coins(29)
+
+
+
+  
+
