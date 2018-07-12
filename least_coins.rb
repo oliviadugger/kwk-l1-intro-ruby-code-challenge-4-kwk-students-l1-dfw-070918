3578337dd18c8e4846c8f6bb5a38f4f89ad682be
oliviadugger <oliviacdugger@gmail.com>
2018-07-12 14:10:28 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -6,22 +6,22 @@ least_coins_hash = {:pennies => 1, :nickels => 5, :dimes => 10, :quarters => 25}
 
   while cents >= 25
   least_coins_hash[:quarters]+=1
-  cents = cents-25
+  cents = cents - 25
   end
   
   while cents >= 10
   least_coins_hash[:dimes]+=1
-  cents = cents-10
+  cents = cents - 10
   end
   
   while cents >= 5
   least_coins_hash[:nickel]+=1
-  cents = cents-5
+  cents = cents - 5
   end
   
   while cents >= 1
   least_coins_hash[:pennies]+=1
-  cents = cents-1
+  cents = cents - 1
   end
 end
 
