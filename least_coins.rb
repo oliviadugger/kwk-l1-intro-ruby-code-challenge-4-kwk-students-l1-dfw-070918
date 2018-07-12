9a7b58eb16a13389ab8587f172bee1ae3db4c83a
oliviadugger <oliviacdugger@gmail.com>
2018-07-12 13:57:28 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,33 @@
 #write out your code here
 
-def least_coins(cents)
+def least_coins(coins)
+  
+least_coins_hash = {:pennies => 1, :nickels => 5, :dimes => 10, :quarters => 25}
 
-#Code your answer here!
-
+  while coins >= 25
+  least_coins_hash[:quarters]+=1
+  coins = coins-25
+  end
+  
+  while coins >= 10
+  least_coins_hash[:dimes]+=1
+  coins = coins-10
+  end
+  
+  while coins >= 5
+  least_coins_hash[:nickel]+=1
+  coins = coins-5
+  end
+  
+  while coins >= 1
+  least_coins_hash[:pennies]+=1
+  coins = coins-1
+  end
 end
+
+least_coins
+
+
+
+  
+
